c5fbc408e4054dcb622a7c9763d1610434991f88
AlexMorenoDev <alx.moreno.lobato@gmail.com>
2024-02-26 20:09:41 +0100

    Challenge 'Recursion': Added new fibonacci function much more optimized

--- Recursion/main.py
+++ Recursion/main.py
@@ -20,19 +20,36 @@ def calculate_factorial(n: int):
     return n * calculate_factorial(n - 1)
 
 
-def get_fibonacci_number(pos: int):
+def get_fibonacci_number_1(pos: int):
     if pos == 0:
         return 0
     if pos == 1:
         return 1
     
-    return get_fibonacci_number(pos - 1) + get_fibonacci_number(pos - 2)
+    return get_fibonacci_number_1(pos - 1) + get_fibonacci_number_1(pos - 2)
+
+
+# Alternative function using a dictionary to avoid calculating numbers that has been already calculated
+# With high numbers it is very efficient
+def get_fibonacci_number_2(pos: int, calculated_nums: dict):
+    if pos == 0:
+        return 0
+    if pos == 1:
+        return 1
+    
+    if pos in calculated_nums:
+        return calculated_nums[pos]
+    else:
+        current_pos_num = get_fibonacci_number_2(pos - 1, calculated_nums) + get_fibonacci_number_2(pos - 2, calculated_nums)
+        calculated_nums[pos] = current_pos_num
+        return current_pos_num
 
 
 def main():
     print_reverse_nums(100)
     print(calculate_factorial(5))
-    print(get_fibonacci_number(10))
+    print(get_fibonacci_number_1(40)) # Execution time: 18-19 seconds
+    print(get_fibonacci_number_2(40, {})) # Execution time: Instantly
 
 
 if __name__ == "__main__":
